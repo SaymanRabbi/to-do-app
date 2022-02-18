6381da74e6eea115848fd9ab4c4af910919f93d0
sayman <mdrabbi.asm@gmail.com>
2022-02-18 22:13:26 +0600

    to do app

--- toDoapp/toDo.js
+++ toDoapp/toDo.js
@@ -1,6 +1,5 @@
 const input = document.getElementById('input');
 const AddItem = document.getElementById('Add-item');
-
 let number = 0;
 const tBody =document.getElementById('tbody');
 AddItem.addEventListener('click',function(e){
@@ -9,13 +8,11 @@ AddItem.addEventListener('click',function(e){
         alert("Please Input Some String")
     }
     else {
-    number++;
-
-
+         number++;
     //   =====create Html Element
       const tableRow = document.createElement('tr');
        tableRow.innerHTML =` <th>${number}</th>
-             <th class="name">${storeInputValue}</th>
+             <th class="storeValue">${storeInputValue}</th>
              <th> <button class="btn btn-primary py-2 px-2 me-3 add">Save</button><button class="btn btn-danger py-2 px-2 me-3 remove" >Remove</button><button class="btn btn-success py-2 px-2  edit">Edit</button></th>`
             tBody.appendChild(tableRow);
         // clear value=======
@@ -24,22 +21,22 @@ AddItem.addEventListener('click',function(e){
         const remove = document.getElementsByClassName('remove');
            for(const removes of remove){
                removes.addEventListener("click",function(e){
-                   e.stopImmediatePropagation();
                  e.target.parentNode.parentNode.style.display="none"
                })
            } 
     //    edit value=====
        const edit = document.getElementsByClassName('edit');
-       const name = document.getElementsByClassName('name')
         for (const edits of edit){
             edits.addEventListener('click',function(e){
-                e.stopImmediatePropagation();
                 e.target.parentNode.parentNode.style.display="none";
-               for(const names of name){  
-                input.value =names.innerText;
-               }  
+                input.value = ''
+                const gotChild = e.target.parentNode.parentNode.childNodes[3].innerText;
+                editName(gotChild);
             })
         } 
     }   
 })
+function editName(value){
+            input.value =  value;
+}
  
